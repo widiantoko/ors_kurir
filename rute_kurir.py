6a6bb617cbbc541ae2eb3f9d6d30e3c1798de803
widiantoko <dodiwidiantoko@gmail.com>
2024-06-05 15:04:29 +0700

    jam

--- rute_kurir.py
+++ rute_kurir.py
@@ -96,8 +96,8 @@ for loc in coords_tuples:
                 folium.Marker(location=loc, icon=folium.Icon(color='green', icon_color='white', prefix='fa', icon='envelope', shadow_size=(0,0))).add_to(mx)
 
        
-#for loc in rec_3:
-#        folium.Marker(location=[loc['Lat_dest'], loc['Long_dest']], tooltip=loc['No Connote'], icon = folium.Icon(color='green', icon_color='white', prefix='fa', icon='envelope', shadow_size=(0,0))).add_to(mx)
+for loc in pin_new:
+        folium.Marker(location=[loc['koord']], tooltip=loc['jam'], icon = folium.Icon(color='green', icon_color='white', prefix='fa', icon='envelope', shadow_size=(0,0))).add_to(mx)
 
 
 
