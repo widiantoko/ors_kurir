25e7cf86184fb8b1b9df873037c701e756e3876b
widiantoko <dodiwidiantoko@gmail.com>
2024-06-05 07:56:15 +0700

    tset

--- rute_kurir.py
+++ rute_kurir.py
@@ -85,8 +85,8 @@ for loc in coords_tuples:
         for cn in konid:
                 folium.Marker(location=loc, 
                               tooltip=f"""<p style='color:#3288bd; text-align:center; 
-                              border-radius:3px; font-size:12px; line-height:1px; padding-top:8px'>{cn}""", shadow_size=(0,0),
-                              icon = folium.Icon(color='green', icon_color='white', prefix='fa', icon='envelope')).add_to(mx)
+                              border-radius:3px; font-size:12px; line-height:1px; padding-top:8px'>{cn}""", 
+                              icon = folium.Icon(color='green', icon_color='white', prefix='fa', icon='envelope', shadow_size=(0,0))).add_to(mx)
 
 
 
