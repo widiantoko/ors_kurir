0537e0223d7857de403f0419f22a15dd121495e1
widiantoko <dodiwidiantoko@gmail.com>
2024-06-05 15:15:28 +0700

    ui

--- rute_kurir.py
+++ rute_kurir.py
@@ -67,7 +67,6 @@ text_Cito=f"""<p style='color:#3288bd; text-align:center; border-radius:3px;
         font-size:12px; line-height:1px; padding-top:8px'>CitoXpress"""
 
 for x in konid:
-
         text2=f"""<p style='color:#3288bd; text-align:center; border-radius:3px; 
         font-size:12px; line-height:3px; padding-top:8px'>{x}"""
 
