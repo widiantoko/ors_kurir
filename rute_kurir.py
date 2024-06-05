4cb23d2dffa0f3db7cc6dd8823db680b47108e73
widiantoko <dodiwidiantoko@gmail.com>
2024-06-05 19:39:11 +0700

    strf

--- rute_kurir.py
+++ rute_kurir.py
@@ -28,6 +28,8 @@ rute_kiriman= data_kurir.apply(lambda row: f"{row['Long_dest']},{row['Lat_dest']
 rute_kurir = ';'.join(rute_kiriman)
 
 
+new['Tgl']=new['Tgl'].strftime("%d-%m-%Y")
+
 opt_kurir = st.selectbox(
     "Nama Kurir:",
     new['Nama Kurir'].drop_duplicates(keep='last'))
