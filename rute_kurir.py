aac512257cf5bcae5d0078fc925925b52b6f8a45
widiantoko <dodiwidiantoko@gmail.com>
2024-06-05 07:47:16 +0700

    ubah

--- rute_kurir.py
+++ rute_kurir.py
@@ -54,7 +54,7 @@ mx = folium.Map(location=cito_loc, zoom_start=12)
 
 
 text_Cito=f"""<p style='color:#3288bd; text-align:center; border-radius:3px; 
-        font-size:12px; line-height:3px; padding-top:8px'>CitoXpress"""
+        font-size:12px; line-height:1px; padding-top:8px'>CitoXpress"""
 
 
 
@@ -84,7 +84,7 @@ for loc in coords_tuples:
         for cn in konid:
                 folium.Marker(location=loc, 
                               tooltip=f"""<p style='color:#3288bd; text-align:center; 
-                              border-radius:3px; font-size:12px; line-height:3px; padding-top:8px'>{cn}""",
+                              border-radius:3px; font-size:12px; line-height:1px; padding-top:8px'>{cn}""",
 
                               icon = folium.Icon(color='green', icon_color='white',prefix='fa', icon='envelope')
                       ).add_to(mx)
