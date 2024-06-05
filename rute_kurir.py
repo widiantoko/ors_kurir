b29e3bafd9d8926965403399c160b211e34365fe
widiantoko <dodiwidiantoko@gmail.com>
2024-06-05 22:50:10 +0700

    coba

--- rute_kurir.py
+++ rute_kurir.py
@@ -43,7 +43,7 @@ mask = (new['Nama Kurir']== opt_kurir) & (new['Tgl']==opt_tgl)
 new_data = new[mask]
 #st.dataframe(new_data)
 
-st.table(new)
+st.table(new_data)
 
 new_data_kiriman= new_data.apply(lambda row: f"{row['Long']},{row['Lat']}", axis=1).tolist()
 new_data_kurir = ';'.join(new_data_kiriman)
