179f5ed5e06fc6c259dd20205abb334ef23752cd
widiantoko <dodiwidiantoko@gmail.com>
2024-06-07 14:09:44 +0700

    new

--- rute_kurir.py
+++ rute_kurir.py
@@ -18,7 +18,7 @@ new=pd.read_excel('data/new.xlsx')
 
 
 
-col1, col2 = st.columns(2)
+
 
 
 cito_lat='106.812288,-6.210011;'
@@ -30,17 +30,17 @@ urut_tgl=new['Tgl'].dt.strftime("%d-%b-%Y").sort_values(ascending=True).drop_dup
 
 new['Tgl']=new['Tgl'].dt.strftime("%d-%b-%Y")
 
-with col1:
-        opt_kurir = st.selectbox("Nama Kurir:",urut_nama)
-        opt_tgl = st.selectbox("Tanggal Delivery:", urut_tgl)
 
-
-with col2:
+opt_kurir = st.selectbox("Nama Kurir:",urut_nama)
+opt_tgl = st.selectbox("Tanggal Delivery:", urut_tgl)
 
 
 
-        mask = (new['Nama Kurir']== opt_kurir) & (new['Tgl']==opt_tgl)
-        new_data = new[mask]        
+
+
+
+mask = (new['Nama Kurir']== opt_kurir) & (new['Tgl']==opt_tgl)
+new_data = new[mask]        
 
 
 x=len(new_data)        
