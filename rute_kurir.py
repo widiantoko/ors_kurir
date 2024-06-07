903def300718ec7413cae13f5bc7fd2a1b50c8aa
widiantoko <dodiwidiantoko@gmail.com>
2024-06-07 17:13:48 +0700

    ta

--- rute_kurir.py
+++ rute_kurir.py
@@ -9,11 +9,11 @@ import pandas as pd
 
 
 
-
+st.set_page_config(page_title = "Simulasi Rute", layout="centered")
 
 
 with st.container():
-        st.set_page_config(page_title = "Simulasi Rute", layout="centered")
+      
         st.subheader("Simulasi Rute Delivery Kurir")
 
 
