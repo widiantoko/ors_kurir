35e089a58731fce9a7e76fe600b794464cf1c4ea
widiantoko <dodiwidiantoko@gmail.com>
2024-06-25 15:38:36 +0700

    coba

--- rute_kurir.py
+++ rute_kurir.py
@@ -24,7 +24,7 @@ with st.container():
 
 
         urut_nama= new['Nama Kurir'].drop_duplicates(keep='last').sort_values(ascending=True)
-        urut_tgl=new['Tgl'].dt.strftime("%d-%b-%Y").drop_duplicates(keep='last').sort_values(by='Tgl') 
+        urut_tgl=new['Tgl'].dt.strftime("%d-%b-%Y").drop_duplicates(keep='last').sort_values(by='Tgl', ascending=False) 
 
         new['Tgl']=new['Tgl'].dt.strftime("%d-%b-%Y")
 
